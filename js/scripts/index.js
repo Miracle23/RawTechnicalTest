a8c8fb4a78de3aaa8b5807464e856858420ca471
Miracle23 <Gouldy_thomas@hotmail.com>
2018-02-13 17:39:54 +0000

    some style checkin

--- js/scripts/index.js
+++ js/scripts/index.js
@@ -17,12 +17,7 @@ function populate(data) {
     $.each(data, function (i, item) {
         var image = $("<img class='responsive-image b-lazy'>").attr("data-src", item.urls.full);
         image.attr("src", item.urls.thumb)
-        if(item.likes >= 50){
-            var item = $("<a class='post cell popularImage'>").append(image);
-        }
-        else{
-            var item = $("<a class='post cell nonPopularImage'>").append(image);
-        }
+        var item = $("<a href='" + item.urls.full + "'>").append(image);
         var $container = $('#page' + page);
         $container.append(item);
     });
@@ -41,12 +36,12 @@ function searchImages(menu) {
     var search;
     if (menu.value == '1') {
         search = "none";
-      } else if (menu.value == '2') {
+    } else if (menu.value == '2') {
         search = "nature";
-      } else if (menu.value == '3') {
+    } else if (menu.value == '3') {
         search = "industry";
-      }
-    
+    }
+
     page = 1;
     $('#posts').empty();
     unSplash.getUnsplash(page, limit, search).then((data) => populate(data))
